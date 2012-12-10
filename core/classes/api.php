aa795c410f3c35240f503d5806fe74ab73220d81
logikal16 <logikal16@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-10 13:18:20 +0000

    CFS 1.7.9
    git-svn-id: https://plugins.svn.wordpress.org/custom-field-suite/trunk@636566 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- core/classes/api.php
+++ core/classes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-class cfs_Api
+class cfs_api
 {
     public $parent;
     public $cache;
@@ -82,7 +82,7 @@ class cfs_Api
         $field_data = array();
 
         // Get all field groups for this post
-        $group_ids = $this->parent->get_matching_groups($post_id, true);
+        $group_ids = $this->get_matching_groups($post_id, true);
 
         if (!empty($group_ids))
         {
@@ -282,7 +282,7 @@ class cfs_Api
         $post_id = empty($post_id) ? $post->ID : (int) $post_id;
 
         // Get all field groups for this post
-        $group_ids = $this->parent->get_matching_groups($post_id, true);
+        $group_ids = $this->get_matching_groups($post_id, true);
 
         $labels = array();
 
@@ -378,6 +378,89 @@ class cfs_Api
 
     /*--------------------------------------------------------------------------------------
     *
+    *    get_matching_groups
+    *
+    *    @author Matt Gibbs
+    *    @since 1.0.0
+    *
+    *-------------------------------------------------------------------------------------*/
+
+    function get_matching_groups($post_id, $skip_roles = false)
+    {
+        global $wpdb, $current_user;
+
+        // Get variables
+        $matches = array();
+        $post_id = (int) $post_id;
+        $post_type = get_post_type($post_id);
+        $page_template = get_post_meta($post_id, '_wp_page_template', true);
+        $user_roles = $current_user->roles;
+        $term_ids = array();
+
+        // Get all term ids associated with this post
+        $sql = "
+        SELECT tt.term_id
+        FROM $wpdb->term_taxonomy tt
+        INNER JOIN $wpdb->term_relationships tr ON tr.term_taxonomy_id = tt.term_taxonomy_id AND tr.object_id = %d";
+        $results = $wpdb->get_results($wpdb->prepare($sql, $post_id));
+        foreach ($results as $result)
+        {
+            $term_ids[] = $result->term_id;
+        }
+
+        // Get all rules
+        $sql = "
+        SELECT p.ID, p.post_title, m.meta_value AS rules
+        FROM $wpdb->posts p
+        INNER JOIN $wpdb->postmeta m ON m.post_id = p.ID AND m.meta_key = 'cfs_rules'
+        WHERE p.post_status = 'publish'";
+        $results = $wpdb->get_results($sql);
+
+        $rule_types = array(
+            'post_types' => $post_type,
+            'user_roles' => $user_roles,
+            'term_ids' => $term_ids,
+            'post_ids' => $post_id,
+            'page_templates' => $page_template,
+        );
+
+        // Ignore user_roles if used within get_fields
+        if (false !== $skip_roles)
+        {
+            unset($rule_types['user_roles']);
+        }
+
+        foreach ($results as $result)
+        {
+            $fail = false;
+            $rules = unserialize($result->rules);
+
+            foreach ($rule_types as $rule_type => $value)
+            {
+                if (isset($rules[$rule_type]))
+                {
+                    $operator = (array) $rules[$rule_type]['operator'];
+                    $in_array = (0 < count(array_intersect((array) $value, $rules[$rule_type]['values'])));
+                    if (($in_array && '!=' == $operator[0]) || (!$in_array && '==' == $operator[0]))
+                    {
+                        $fail = true;
+                    }
+                }
+            }
+
+            if (!$fail)
+            {
+                $matches[$result->ID] = $result->post_title;
+            }
+        }
+
+        // Allow for overrides
+        return apply_filters('cfs_matching_groups', $matches, $post_id, $post_type);
+    }
+
+
+    /*--------------------------------------------------------------------------------------
+    *
     *    save_fields
     *
     *    @author Matt Gibbs
@@ -428,7 +511,7 @@ class cfs_Api
         }
         elseif ('api' == $options->format)
         {
-            $group_ids = $this->parent->get_matching_groups($post_id, true);
+            $group_ids = $this->get_matching_groups($post_id, true);
             $group_ids = array_keys($group_ids);
         }
 
@@ -614,5 +697,139 @@ class cfs_Api
                 $this->save_fields_recursive($new_params);
             }
         }
+    }
+
+
+    /*--------------------------------------------------------------------------------------
+    *
+    *    save_field_group
+    *
+    *    @author Matt Gibbs
+    *    @since 1.8.0
+    *
+    *-------------------------------------------------------------------------------------*/
+
+    function save_field_group($params = array())
+    {
+        global $wpdb;
+
+        $params = (object) $params;
+        $post_id = $params->post_id;
+
+        /*---------------------------------------------------------------------------------------------
+            Save fields
+        ---------------------------------------------------------------------------------------------*/
+
+        $weight = 0;
+        $prev_fields = array();
+        $current_field_ids = array();
+        $table_name = $wpdb->prefix . 'cfs_fields';
+
+        // Get existing fields (check for renamed or deleted fields)
+        $results = $wpdb->get_results("SELECT id, name FROM $table_name WHERE post_id = '$post_id'");
+        foreach ($results as $result)
+        {
+            $prev_fields[$result->id] = $result->name;
+        }
+
+        // Remove all existing fields
+        $wpdb->query("DELETE FROM $table_name WHERE post_id = '$post_id'");
+
+        foreach ($params->fields as $key => $field)
+        {
+            // Sanitize the field
+            $field = stripslashes_deep($field);
+
+            // Allow for field customizations
+            $field = $this->parent->fields[$field['type']]->pre_save_field($field);
+
+            // Save empty string for fields without options
+            $field['options'] = !empty($field['options']) ? serialize($field['options']) : '';
+
+            $data = array(
+                'name' => $field['name'],
+                'label' => $field['label'],
+                'type' => $field['type'],
+                'instructions' => $field['instructions'],
+                'post_id' => $post_id,
+                'parent_id' => $field['parent_id'],
+                'weight' => $weight,
+                'options' => $field['options'],
+            );
+
+            // Use an existing ID if available
+            if (0 < (int) $field['id'])
+            {
+                $data['id'] = (int) $field['id'];
+
+                // We use this variable to check for deleted fields
+                $current_field_ids[] = $data['id'];
+
+                // Rename the postmeta key if necessary
+                if ($field['name'] != $prev_fields[$data['id']])
+                {
+                    $wpdb->query(
+                        $wpdb->prepare("
+                            UPDATE {$wpdb->postmeta} m
+                            INNER JOIN {$wpdb->prefix}cfs_values v ON v.meta_id = m.meta_id
+                            SET meta_key = %s
+                            WHERE v.field_id = %d",
+                            $field['name'], $data['id']
+                        )
+                    );
+                }
+            }
+
+            // Insert the field
+            $wpdb->insert($table_name, $data);
+
+            $weight++;
+        }
+
+        // Remove values for deleted fields
+        $deleted_field_ids = array_diff(array_keys($prev_fields), $current_field_ids);
+
+        if (0 < count($deleted_field_ids))
+        {
+            $deleted_field_ids = implode(',', $deleted_field_ids);
+            $wpdb->query("
+                DELETE v, m
+                FROM {$wpdb->prefix}cfs_values v
+                INNER JOIN {$wpdb->postmeta} m ON m.meta_id = v.meta_id
+                WHERE v.field_id IN ($deleted_field_ids)"
+            );
+        }
+
+        /*---------------------------------------------------------------------------------------------
+            Save rules
+        ---------------------------------------------------------------------------------------------*/
+
+        $data = array();
+        $rule_types = array('post_types', 'user_roles', 'post_ids', 'term_ids', 'page_templates');
+
+        foreach ($rule_types as $type)
+        {
+            if (!empty($params->rules[$type]))
+            {
+                // Break apart the autocomplete string
+                if ('post_ids' == $type)
+                {
+                    $params->rules[$type] = explode(',', $params->rules[$type]);
+                }
+
+                $data[$type] = array(
+                    'operator' => $params->rules['operator'][$type],
+                    'values' => $params->rules[$type],
+                );
+            }
+        }
+
+        update_post_meta($post_id, 'cfs_rules', $data);
+
+        /*---------------------------------------------------------------------------------------------
+            Save extras
+        ---------------------------------------------------------------------------------------------*/
+
+        update_post_meta($post_id, 'cfs_extras', $params->extras);
     }
 }
